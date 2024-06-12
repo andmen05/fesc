65601fa5cb2f080cbfd7ba9d92b073ab786c9231
andmen05 <132867568+andmen05@users.noreply.github.com>
2024-06-12 10:17:26 -0500

    Add files via upload

---
+++ storage/framework/views/62d959958a6d4be406843cd72696c35992525bf2.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="../assets/css/nucleo-icons.css" rel="stylesheet">
+    <link href="../assets/css/nucleo-svg.css" rel="stylesheet">
+    <link rel="stylesheet" href="../assets/css/soft-ui-dashboard.min.css">
+    <script src="../assets/js/soft-ui-dashboard.js"></script>
+    <script src="../assets/js/core/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+    <title>Registros De Empleado</title>
+</head>
+<body>
+    <div class="row justify-content-center">
+        <h1 class="col col-lg-3">
+            <span class="card-label fw-bold text-dark"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Registros</font></font></span>
+        </h1>
+    </div> 
+    <div class="row">
+        <div class="card mt-4">
+            <div class="card-body">
+                <!--begin::Table-->
+                <div id="kt_table_widget_5_table_wrapper" class="dataTables_wrapper dt-bootstrap4 no-footer">
+                    <div class="table-responsive">
+                        <table class="table align-middle table-row-dashed fs-6 gy-3 dataTable no-footer" id="kt_table_widget_5_table">
+                    
+                    <thead>
+                        <tr class="text-center text-gray-400 fw-bold fs-7 text-uppercase gs-0">
+                            <th class="min-w-150px sorting sorting_desc" tabindex="0" aria-controls="kt_table_widget_5_table" rowspan="1" colspan="1" aria-label="Item: activate to sort column ascending" style="width: 182.922px;" aria-sort="descending"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">NOMBRE</font></font></th>
+                            <th class="text-center pe-3 min-w-100px sorting_disabled" rowspan="1" colspan="1" aria-label="Product ID" style="width: 380.219px;"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">TIPO DE PERMISO</font></font></th>
+                            <th class="text-center pe-3 min-w-100px sorting_disabled" rowspan="1" colspan="1" aria-label="Product ID" style="width: 380.219px;"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">FECHA DE INICIO</font></font></th>
+                            <th class="text-center pe-3 min-w-150px sorting" tabindex="0" aria-controls="kt_table_widget_5_table" rowspan="1" colspan="1" aria-label="Date Added: activate to sort column ascending" style="width: 350.203px;"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">APELLIDO</font></font></th>
+                            <th class="text-center pe-3 min-w-100px sorting" tabindex="0" aria-controls="kt_table_widget_5_table" rowspan="1" colspan="1" aria-label="Price: activate to sort column ascending" style="width: 350.219px;"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">NUMERO ID</font></font></th>
+                            <th class="text-center pe-3 min-w-100px sorting" tabindex="0" aria-controls="kt_table_widget_5_table" rowspan="1" colspan="1" aria-label="Price: activate to sort column ascending" style="width: 350.219px;"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">TELÉFONO</font></font></th>
+                            <th class="text-center pe-3 min-w-100px sorting" tabindex="0" aria-controls="kt_table_widget_5_table" rowspan="1" colspan="1" aria-label="Price: activate to sort column ascending" style="width: 350.219px;"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">CORREO</font></font></th>
+                            <th class="text-center pe-3 min-w-100px sorting" tabindex="0" aria-controls="kt_table_widget_5_table" rowspan="1" colspan="1" aria-label="Price: activate to sort column ascending" style="width: 350.219px;"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">COLEGIO</font></font></th>
+                            <th class="text-end pe-3 min-w-100px sorting" tabindex="0" aria-controls="kt_table_widget_5_table" rowspan="1" colspan="1" aria-label="Status: activate to sort column ascending" style="width: 300.703px;"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;"></font></font></th>
+                        </tr>
+                    </thead>
+                    
+                    <tbody class="fw-bold text-gray-600">
+
+                        
+                            <tr class="odd">
+                                <td class="text-center" data-order="2023-04-20T00:00:00-05:00">
+                                    <div class="mb-3">
+                                        
+                                        
+                                    </div>
+                                </td>
+                                
+                               
+                                 <td class="text-center ">
+                                    
+                                </td>
+
+                                <!--begin::Product ID-->
+                                <i class="ki-outline ki-dots-square fs-1 text-gray-400 me-n1"></i>
+                                <td class="text-center ">
+                                    
+                                </td>
+                                <!--end::Product ID-->
+                
+                                <!--begin::Date added-->
+                                <td class="text-center">
+                                    
+                                </td>
+                                <!--end::Date added-->
+                
+                                <!--begin::Price-->
+                                <td class="text-center">
+                                    
+                                </td>
+
+                                <td class="text-center">
+                                    
+                                </td>
+                                <!--end::Price-->
+                
+                                <!--begin::Status-->
+                                
+                                <td class="text-center">
+                                    
+                                </td>
+                                <!--end::Status-->
+                            
+                            </tr>
+                        
+                    </tbody>
+                    
+                    <!--end::Table body-->
+                </table>
+            </div>
+            
+        </div>
+        
+    </div>
+</body>
+</html><?php /**PATH C:\xampp\htdocs\Fesc\resources\views/Admin/vista.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/9d4cf2cc71e4d5f6d317052644915d587b6c76cd.php
@@ -0,0 +1,19 @@
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="<?php echo e(asset ("/assets/css/nucleo-icons.css")); ?>" rel="stylesheet">
+        <link href="<?php echo e(asset ("/assets/css/nucleo-svg.css")); ?>" rel="stylesheet">
+        <link rel="stylesheet" href="<?php echo e(asset ("/assets/css/soft-ui-dashboard.min.css")); ?>">
+        <title>Document</title>
+    </head>
+    <body>
+        <form class="form">
+            <p class="heading">Login</p>
+            <input class="input" placeholder="Username" type="text">
+            <input class="input" placeholder="Password" type="text"> 
+            <button class="btn">Submit</button>
+        </form>
+
+    </body>
+</html><?php /**PATH C:\xampp\htdocs\Fesc\resources\views/Admin/adminingreso.blade.php ENDPATH**/ ?>
